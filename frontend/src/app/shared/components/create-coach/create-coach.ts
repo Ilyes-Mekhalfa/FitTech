906a956570e071801af25b4efdcb415b9d1de564
Ilyes Mekhalfa <ilyes.mekalfa@gmail.com>
2026-04-21 14:55:08 +0100

    feat(coach): add add and preview coach pages

--- frontend/src/app/shared/components/create-coach/create-coach.ts
+++ frontend/src/app/shared/components/create-coach/create-coach.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CoachService } from '../../../core/services/coach.service';
 @Component({
   selector: 'app-create-coach',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-coach.html',
   styleUrl: './create-coach.css',
 })
 export class CreateCoach {
+  createCoachForm: FormGroup;
 
+  //to be continued later
+  constructor(private fb: FormBuilder, private coachService: CoachService, private router: Router){
+    this.createCoachForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phone: ['', Validators.required, Validators.pattern('^[0-9]{8}$')],
+      password: ['', Validators.required, Validators.minLength(8)],
+      confirm_pw: ['', Validators.required],
+      biography: ['', Validators.required],
+      specialties: ['', Validators.required],
+      experience: ['', Validators.required],
+    })
+  }
+
+  submit(){
+    if (this.createCoachForm.invalid) {
+      throw new Error('form is invalid')
+    }
+  }
+
+  exit(){
+    this.router.navigate(['/coachs'])
+  }
 }
