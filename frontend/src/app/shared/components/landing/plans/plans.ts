8f76bc5101ddf5dfa72bba102ccd244ca135396c
Ilyes Mekhalfa <ilyes.mekalfa@gmail.com>
2026-05-31 00:10:16 +0100

    feat(landing page): add plans and coaches sections add footer component

--- frontend/src/app/shared/components/landing/plans/plans.ts
+++ frontend/src/app/shared/components/landing/plans/plans.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-
+import { LandingService } from '../../../../core/services/landing.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-plans',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './plans.html',
   styleUrl: './plans.css',
 })
 export class Plans {
+  allPlans: any[] = [];
+  filteredPlans: any[] = [];
+  activeFilter:  'daily' | 'monthly' | 'quarterly' | 'yearly' = 'monthly';
 
+  constructor(private landingService: LandingService) {}
+
+  ngOnInit() {
+    this.landingService.getPlansData().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        
+        this.allPlans =  res;
+        this.setPlanFilter('monthly')
+      },
+      error: (err) => {
+        console.log('error fetching plans data', err);
+      }
+    });
+  }
+
+  setPlanFilter(filter: 'daily' | 'monthly' | 'quarterly' | 'yearly') {
+    this.activeFilter = filter;
+    this.filteredPlans = this.allPlans.filter((e)=> e.type == filter)
+  }
 }
